c9799a14f9a9e6dabfae3eb0a51b8cf39fa54371
papreethi03-pixel <papreethi03@gmail.com>
2025-11-16 20:31:14 +0530

    Update __init__.py

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -7,10 +7,18 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_session import Session
+from flask.logging import create_logger
 
 app = Flask(__name__)
 app.config.from_object(Config)
 # TODO: Add any logging levels and handlers with app.logger
+# DONE (Note: Instructor's original app.logger code not pylint compliant)
+LOG = create_logger(app)
+LOG.setLevel(logging.INFO)
+streamHandler = logging.StreamHandler()
+streamHandler.setLevel(logging.INFO)
+LOG.addHandler(streamHandler)
+
 Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
